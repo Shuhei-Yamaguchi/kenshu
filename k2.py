1397aa515f56495dac510a002b30c8e937b93686
yoshiscythe <yoshiscythe@gmail.com>
2018-12-05 14:52:59 +0900

    ver12053

--- k2.py
+++ k2.py
@@ -5,8 +5,8 @@ def Help():
   return '''Template of script.
   Usage: template'''
 def Run(ct,*args):
-  t1=8
-  t2=3
+  t1=4
+  t2=1
 
   def quaternion(axis,angle) :
 	return [i*math.sin(angle/2) for i in axis]+[math.cos(angle/2)]
@@ -17,48 +17,48 @@ def Run(ct,*args):
   q3 = list(MultiplyQ(q1,q2))
 
   x1=0.5 #最初に拾うジェンガの中心座標
-  y1=-0.1
-  z1=0.2
+  y1=0.1
+  z1=0.295
 
   x2=0.3 #最初に置く段の真ん中のジェンガの中心座標
   y2=-0.2
-  z2=0.1
+  z2=0.295
   
-  thick=0.074
+  thick=0.075
   h=0.015
   w=0.025
 
   def pickup(a): #ジェンガを所定位置から拾う
-   ct.robot.MoveToX([x1,y1+a*w,z1+0.1]+q1,t2,blocking=True) 
+   ct.robot.MoveToX([x1,y1+a*w,z1+0.1]+q3,t1,blocking=True) 
    rospy.sleep(t2)
    ct.robot.OpenGripper()
    rospy.sleep(t2)
-   ct.robot.MoveToX([x1,y1+a*w,z1]+q1,t2, blocking=True)#一回ごとにwだけｙ軸側に拾う位置をずらします
+   ct.robot.MoveToX([x1,y1+a*w,z1]+q3,t1,blocking=True)#一回ごとにwだけｙ軸側に拾う位置をずらします
    rospy.sleep(t2)
    ct.robot.MoveGripper(thick)
    rospy.sleep(t2)
-   ct.robot.MoveToX([x1,y1+a*w,z1+0.1]+q1,t2, blocking=True)
+   ct.robot.MoveToX([x1,y1+a*w,z1+0.1]+q3,t1, blocking=True)
    rospy.sleep(t2)
 
 #段が変わるごとに置く位置をz軸方向にhだけずらします
   def move1(a,b):#姿勢１の状態で積む位置に移動し置く
-   ct.robot.MoveToX([x2+w*a,y2,z2+0.1+2*b*h]+q1,t2,blocking=True)#同じ段では一回ごとにwだけx軸に沿って置く位置をずらします
+   ct.robot.MoveToX([x2+w*a,y2,z2+0.1+2*b*h]+q1,t1,blocking=True)#同じ段では一回ごとにwだけx軸に沿って置く位置をずらします
    rospy.sleep(t2)
-   ct.robot.MoveToX([x2+w*a,y2,z2+2*b*h]+q1,t2,blocking=True) 
+   ct.robot.MoveToX([x2+w*a,y2,z2+2*b*h]+q1,t1,blocking=True) 
    rospy.sleep(t2)
    ct.robot.OpenGripper()
    rospy.sleep(t2)
-   ct.robot.MoveToX([x2+w*a,y2,z2+0.1+2*b*h]+q1,t2,blocking=True) 
+   ct.robot.MoveToX([x2+w*a,y2,z2+0.1+2*b*h]+q1,t1,blocking=True) 
    rospy.sleep(t2)
 
   def move3(a,b): #姿勢３の状態で積む位置に移動し置く
-   ct.robot.MoveToX([x2,y2+w*a,z2+0.1+(1+2*b)*h]+q3,t2,blocking=True)#同じ段では一回ごとにwだけy軸に沿って置く位置をずらします
+   ct.robot.MoveToX([x2,y2+w*a,z2+0.1+(1+2*b)*h]+q3,t1,blocking=True)#同じ段では一回ごとにwだけy軸に沿って置く位置をずらします
    rospy.sleep(t2)
-   ct.robot.MoveToX([x2,y2+w*a,z2+(1+2*b)*h]+q3,t2,blocking=True) 
+   ct.robot.MoveToX([x2,y2+w*a,z2+(1+2*b)*h]+q3,t1,blocking=True) 
    rospy.sleep(t2)
    ct.robot.OpenGripper()
    rospy.sleep(t2)
-   ct.robot.MoveToX([x2,y2+w*a,z2+0.1+(1+2*b)*h]+q3,t2,blocking=True) 
+   ct.robot.MoveToX([x2,y2+w*a,z2+0.1+(1+2*b)*h]+q3,t1,blocking=True) 
    rospy.sleep(t2)
 
   def moveq1():
@@ -72,7 +72,6 @@ def Run(ct,*args):
 
 #ここから実行
   move0()
-  print q1
   for k in range(2):#とりあえず４段積むことにしました
    for i in range(3):
     pickup(6*k+i)
